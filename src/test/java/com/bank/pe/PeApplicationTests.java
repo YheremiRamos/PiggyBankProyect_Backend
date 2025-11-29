2df47e58ccf13c1482c1de34b551c6e2efb68001
summermp <summervelis@gmail.com>
2025-11-29 16:40:03 -0500

    estructura básica del proyecto

---
+++ src/test/java/com/bank/pe/PeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
